314be13df33db18df1a4e09a827d4e4ef63fe639
Randyr902 <rdj7890@gmail.com>
2015-12-01 15:44:29 -0800

    added css files and cleaned up welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :notes
+
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root"
+  end
+
 root 'welcome#index'
-resources :notes
 end
